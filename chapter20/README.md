067e97091a65432b257068eaf5740c6ec032a168
aseelsaleh <aseel.saleh2611@gmail.com>
2020-02-23 12:50:17 +0200

    Chapter 20 - Constructivist

---
+++ chapter20/README.md
@@ -0,0 +1,7 @@
+Defensive programming: 
+- Every single function checks the sanity of its arguments and either returns something sensible when the arguments are unreasonable or assigns them reasonable values.
+- All code blocks check for possible errors and escape the block when things go wrong, setting the state to something reasonable, and continuing to execute the rest of the function.
+
+Notes: 
+- Throw exception where error can occur and solve it.
+- code is clean and understoodable.
---
+++ chapter20/index.ts
@@ -0,0 +1,77 @@
+let fs = require('fs');
+
+function extractWords(path: string) {
+    if (path == "") {
+        return [];
+    }
+    try {
+        return fs.readFileSync(path).toString().replace(/[\W_|]+/gi, " ").split(" ");
+    } catch{
+        console.log("Error in opening the file");
+        return [];
+    }
+}
+
+function removeStopWord(words: string[]) {
+    if (words == [])
+        return [];
+    let stopwords;
+    try {
+        stopwords = fs.readFileSync('../data\\stop_words.txt').toString().split(" ");
+    } catch{
+        console.log("error opening the stop words file")
+        return words;
+    }
+    let newWords = []
+    for (let word in words) {
+        if (!stopwords.includes(words[word])) {
+            newWords.push(words[word]);
+        }
+    }
+    return newWords;
+}
+
+function frequancies(words: string[]) {
+    if (words == []) {
+        return [];
+    }
+
+    let words_count: [string, number][] = [];
+
+    for (let word in words) {
+        let found = false;
+        for (var i = 0; i < words_count.length; i++) {
+            if (words[word] == words_count[i][0]) {
+                words_count[i][1] += 1;
+                found = true
+            }
+        }
+        if (!found) {
+            words_count.push([words[word], 1]);
+        }
+    }
+    return words_count;
+}
+
+function sort(words_count: [string, number][]) {
+    if (words_count == []) {
+        return [];
+    }
+    words_count.sort((a, b) => {
+        return b[1] - a[1];
+    });
+    return words_count;
+}
+
+function main() {
+    let count_word = sort(frequancies(removeStopWord(extractWords('../data\\dummy_data.txt'))));
+    if (count_word instanceof Array) {
+        for (let i = 0; i < count_word.length && i < 25; i++) {
+            console.log(count_word[i]);
+        }
+    } else {
+        console.log("Error!");
+    }
+}
+
+main();
